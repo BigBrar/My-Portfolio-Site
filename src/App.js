152a30dc5d9a7595fcc52eeca6aea9d58a3484c4
Deepinder Brar <deepindersinghdeep03@gmail.com>
2024-10-23 18:59:17 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import Introduction from './components/Introduction';
+import Technologies from './components/Technologies';
+import styles from './index.module.css'
+import Image from './profile.jpg'
+import Heading from './components/Heading';
+import Skills from './components/section 3/Skills';
+import Projects from './components/section 4/Projects';
+
 
 function App() {
   return (
     <div className="App">
+     <Introduction styles={styles} Image={Image}/>
+     <Technologies styles={styles}/>
+     <Heading topMargin={110} styles={styles} headingText='Other Skills'/>
+     <Skills/>
+     <Heading topMargin={110} styles={styles} headingText='My Projects'/>
+     <Projects/>
     </div>
   );
 }
